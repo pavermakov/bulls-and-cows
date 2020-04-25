92425dc381245761f43f5dcc0476ac06b75c964a
Pavel Ermakov <Pavel1994-09@mail.ru>
2020-04-25 13:17:41 +0300

    MOBILE-10 Added splash screen and icons

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
 import Overlay from '~/components/Overlay';
 import Tooltip from '~/components/Tooltip';
 import Farmer from '~/components/Farmer';
 import CloseButton from '~/components/CloseButton';
 
-const Finish = ({ value, onRestart }) => {
+const formatTime = (startTime, endTime) => {
+  const duration = endTime - startTime;
+  const minutes = Math.floor(duration / 60000);
+  const seconds = ((duration % 60000) / 1000).toFixed(0);
+
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+const Finish = ({ value, startTime, endTime, attempts, onRestart }) => {
+
   return (
     <Overlay>
       <View style={s.wrapper}>
@@ -15,7 +26,15 @@ const Finish = ({ value, onRestart }) => {
           <Tooltip style={s.tooltip}>
             <ScrollView contentContainerStyle={s.area}>
               <Text style={s.title}>
-                You have guessed <Text style={s.number}>{value}</Text>!
+                You have guessed <Text style={s.number}>{value}</Text> in:
+              </Text>
+
+              <Text style={s.subtitle}>
+                {attempts} attempts
+              </Text>
+
+              <Text style={s.subtitle}>
+                {formatTime(startTime, endTime)}
               </Text>
             </ScrollView>
           </Tooltip>
@@ -32,6 +51,14 @@ const Finish = ({ value, onRestart }) => {
   );
 };
 
+Finish.propTypes = {
+  value: PropTypes.string.isRequired,
+  attempts: PropTypes.number.isRequired,
+  startTime: PropTypes.number.isRequired,
+  endTime: PropTypes.number.isRequired,
+  onRestart: PropTypes.func.isRequired,
+};
+
 const s = StyleSheet.create({
   wrapper: {
     flex: 1,
@@ -44,9 +71,13 @@ const s = StyleSheet.create({
     minWidth: '100%',
   },
   title: {
-    fontSize: 24,
+    fontSize: 22,
     marginBottom: 5,
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 22,
     textAlign: 'center',
   },
   area: {
